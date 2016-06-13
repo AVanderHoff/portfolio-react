9914b6d1d29f552ea5a2733f0ee1afebe8570b1c
Andrew VanderHoff <andrewvanderhoff@gmail.com>
2016-06-13 00:05:07 -0400

    pictures and links added

--- app/components/Portfolio.js
+++ app/components/Portfolio.js
@@ -3,8 +3,12 @@ var Router = require('react-router');
 var hangman = require('url?!./assets/images/CARHANGMAN.png');
 var starwars = require('url?!./assets/images/star-wars-rpg.png');
 var triviagame = require('url?!./assets/images/trivia-game.png');
-var giffysearch = require('url?!./assets/images/giffy-search.png')
-var trainstation = require('url?!./assets/images/trains.png')
+var giffysearch = require('url?!./assets/images/giffy-search.png');
+var trainstation = require('url?!./assets/images/trains.png');
+var friendfinder = require('url?!./assets/images/friend-finder.png');
+var burgerseq = require('url?!./assets/images/burgerseq.png');
+
+
 
 
 var Portfolio = React.createClass({
@@ -58,14 +62,34 @@ return(
 		<div className= "col-md-4">
 		<a href="http://giffy-search-av.herokuapp.com/" target="_blank" >
 	<img id="giffy-search" src={giffysearch} alt="giffy-search" title="giffy-search" /> </a>
-		<h3>Giffy-Search </h3>
+		<h3>Giffy Search </h3>
 	 </div>
 
 	 <div className= "col-md-4">
 		<a href="http://train-station-av.herokuapp.com/" target="_blank" >
 	<img id="train-station" src={trainstation} alt="train-station" title="train-station" /> </a>
-		<h3>Train-Station </h3>
+		<h3>Train Station </h3>
 	 </div>
+
+	 <div className= "col-md-4">
+		<a href="http://friend-finder-av.herokuapp.com/" target="_blank" >
+	<img id="friend-finder" src={friendfinder} alt="friend-finder" title="friend-finder" /> </a>
+		<h3>Friend Finder </h3>
+	 </div>
+
+	 	 <div className= "col-md-4">
+		<a href="http://burgerseq.herokuapp.com/" target="_blank" >
+	<img id="burger-seq" src={burgerseq} alt="burger-seq" title="burger-seq" /> </a>
+		<h3> Burger Stand Sequelize </h3>
+	 </div>
+
+
+
+
+
+
+
+
 
 </div>
 </div>
